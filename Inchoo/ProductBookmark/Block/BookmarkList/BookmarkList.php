efc28c83d753b0b09c16686d85670ca6a58df962
jkovacevic992 <jkovacevic992@gmail.com>
2019-05-13 13:34:25 +0200

    Added template and block for bookmark lists

---
+++ Inchoo/ProductBookmark/Block/BookmarkList/BookmarkList.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inchoo
+ * Date: 5/13/19
+ * Time: 1:27 PM
+ */
+
+namespace Inchoo\ProductBookmark\Block\BookmarkList;
+
+
+class BookmarkList
+{
+
+}
---
+++ Inchoo/ProductBookmark/Controller/BookmarkList/BookmarkList.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inchoo
+ * Date: 5/13/19
+ * Time: 1:23 PM
+ */
+
+namespace Inchoo\ProductBookmark\Controller\BookmarkList;
+
+
+class BookmarkList
+{
+
+}
